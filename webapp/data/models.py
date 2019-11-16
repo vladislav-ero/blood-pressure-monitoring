339121732edcf04c4d52a715b5ef52d0503c63b3
Vladislav Eroshenko <vladislav400@gmail.com>
2019-11-17 00:14:55 +0300

    Added pressure_category method to Data model

--- webapp/data/models.py
+++ webapp/data/models.py
@@ -20,6 +20,23 @@ class Data(Base):
 
     user = relationship('User', backref='data')
 
+    def blood_pressure_category(self, systolic_pressure, diastolic_pressure):
+        if systolic_pressure <= 120 and diastolic_pressure <= 80:
+            self.pressure_category = 0  # Normal
+            return
+        elif 120 < systolic_pressure <= 129 and diastolic_pressure < 80:
+            self.pressure_category = 1  # Elevated
+            return
+        elif 130 <= systolic_pressure <= 139 or 80 <= diastolic_pressure <= 89:
+            self.pressure_category = 2  # High blood pressure (stage 1)
+            return
+        elif systolic_pressure >= 190 or diastolic_pressure >= 120:
+            self.pressure_category = 4  # Hypertensive crisis
+            return
+        elif 140 <= systolic_pressure < 190 or 90 <= diastolic_pressure < 120:
+            self.pressure_category = 3  # High blood pressure (stage 2)
+            return
+
     def __repr__(self):
         return f"<Value '{self.id}' user '{self.user_id}' \
             '{self.sys_pressure}/{self.dias_pressure}'>"
